3d81a4d70836ead4e21599edbd8b289b0476667e
Sylber22 <40859595+Sylber22@users.noreply.github.com>
2018-07-06 12:08:27 -0300

    2DA CLASE

---
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/jsEntradaSalida-7.js
@@ -0,0 +1,85 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	/*numeroUno="6";
+	numeroDos=6;*/
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	
+	
+	resultado=numeroUno+numeroDos;
+	alert ("la suma es: "+resultado);
+
+}
+
+function restar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	/*numeroUno="6";
+	numeroDos=6;*/
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	
+	
+	resultado=numeroUno-numeroDos;
+	alert ("la resta es: "+resultado);
+}
+
+function multiplicar()
+{ 
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	/*numeroUno="6";
+	numeroDos=6;*/
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	
+	
+	resultado=numeroUno*numeroDos;
+	alert ("la multiplicación es: "+resultado) ;
+}
+
+function dividir()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	/*numeroUno="6";
+	numeroDos=6;*/
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	
+	
+	resultado=numeroUno/numeroDos;
+	alert ("la division es: "+resultado);
+}
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var iva;
+		
+	precio=prompt ("Ingresar precio : ");
+	porcentaje=prompt ("Ingresar porcentaje: ");
+	descuento= (precio*porcentaje)/100;
+	
+	descuento=porcentaje+precio;
+	alert ("Ingresar precio y porcentaje: "+descuento);
 
 }
