07f19bcddde4b25606516300ad5844dfddb76ce2
Sylber22 <40859595+Sylber22@users.noreply.github.com>
2018-07-06 16:51:29 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,13 +2,11 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	///*esto es un comentario de bloque*//*var person = prompt("Please enter your name", "Harry Potter")*/;
-	/*esto lo saquè de w3school*/
-	/*nunca se llama a la variable por un nombre especifico*/
 	var nombre;
-	nombre=" Silvana";
-	alert("su nombre es:"+nombre);
-	nombre = prompt("Ingrese su nombre");
-}
 
+	nombre= "silvana";
+	nombre=prompt ("Ingrese su nombre");
+	alert ("Su nombre es: "+nombre);
+
+}
 
