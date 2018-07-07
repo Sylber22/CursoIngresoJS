b341c3ba74e77fc512e0d4d88b959f8ad7573257
Sylber22 <40859595+Sylber22@users.noreply.github.com>
2018-07-07 19:09:43 -0300

    Practica

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -5,4 +5,18 @@ ej.: "El resto es 0 ."*/
 function SacarResto()
 {
 	
+	var numeroDividendo;
+	var numeroDivisor;
+	var resto;
+
+	numeroDividendo=document.getElementById('numeroDividendo').value;
+	numeroDivisor=document.getElementById ('numeroDivisor').value;
+
+	numeroDividendo=parseInt (numeroDividendo);
+	numeroDivisor= parseInt (numeroDivisor);
+
+	resto=numeroDividendo%numeroDivisor;
+	/* % (signo del resto)*/
+	alert ("El resto es: "+resto);
+
 }
