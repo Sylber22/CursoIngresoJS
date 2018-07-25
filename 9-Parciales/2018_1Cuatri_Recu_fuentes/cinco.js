8917fe83205056c950b0baf180b6a5c600ecdea8
Sylber22 <40859595+Sylber22@users.noreply.github.com>
2018-07-25 09:38:35 -0300

    Switch if

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
-
+	var diaSem= prompt ("Ingrese día de la semana");
+	switch (diaSem)
+	{
+		case "Viernes":
+		case "Sabado":
+		case "Domingo":
+		alert ("Buen finde");
+		break;
+		case "Lunes";
+		case "Martes";
+		case "Miércoles";
+		case "Jueves";	
+		alert ("a trabajar");
+		break;
+		default:
+		alert ("No pertenece a un día dela semana")
+	}
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var numeroUno=prompt ("Ingrese numero uno");
+	var numeroDos=prompt ("Ingrese numero dos");
+	var resultado;
+	if (numeroUno==numeroDos) 
+	{
+		alert ("los numeros son: "+numeroUno+" y "+numeroDos);
+	}
+	else
+	{
+		if (numeroUno<numeroDos)
+		{
+			numeroUno= parseInt (numeroUno);
+			numeroDos= parseInt (numeroDos); 
+			resultado= numeroUno/numeroDos;
 
-}
+		}
+		else 
+		{
+			if (numeroDos<numeroUno)
+			{
+				numeroUno= parseInt (numeroUno);
+				numeroDos= parseInt (numeroDos); 
+				resultado= numeroDos+numeroUno;
+			}
+			
+
+		}
+		if (resultado<50) 
+				{
+					alert ("la suma es "+ resultado +" y es menor a 50");
+				}
+				else
+				{
+						alert (resultado);
+				}
+	}
+	
+			}
+
+
